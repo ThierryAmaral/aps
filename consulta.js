d16660d0a58921968771a5185d10a7c6e972b2ea
ThierryAmaral <thierryamaral433@gmail.com>
2023-06-19 16:04:24 -0300

    Modal

--- consulta.js
+++ consulta.js
@@ -1,7 +1,7 @@
-function abrirJanela(){
-    document.getElementById("popup_consulta").style.display = 'block';
-}
+// function abrirJanela(){
+//     document.getElementById("popup_consulta").style.display = 'block';
+// }
 
-function fecharJanela(){
-    document.getElementById("popup_consulta").style.display = 'none';
-}
+// function fecharJanela(){
+//     document.getElementById("popup_consulta").style.display = 'none';
+// }
